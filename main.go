9c43df00ecfa8080db2c0c42bcb4d60f4e41f61d
lijiyi <lijiyi_321@163.com>
2020-01-09 15:19:09 +0800

    modify errcheck

--- main.go
+++ main.go
@@ -11,35 +11,37 @@ import (
 	"text/template"
 )
 
-func SendHttpRequestDemo() {
-	fmt.Println("vim-go")
-
-	// 自定义请求
-	req, err := http.NewRequest(http.MethodGet, "http://www.baidu.com", nil)
-	if err != nil {
-		panic(err)
-	}
-	req.Header.Add("User-Agent", " Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1")
-
-	response, err := http.DefaultClient.Do(req)
-	//	response, err := http.Get("http://www.baidu.com")
-	//	defer response.Body.Close()
-
-	if err != nil {
-		panic(err)
-	}
-
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(body))
-}
+//func SendHttpRequestDemo() {
+//	fmt.Println("vim-go")
+//
+//	// 自定义请求
+//	req, err := http.NewRequest(http.MethodGet, "http://www.baidu.com", nil)
+//	if err != nil {
+//		panic(err)
+//	}
+//	req.Header.Add("User-Agent", " Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1")
+//
+//	response, err := http.DefaultClient.Do(req)
+//	//	response, err := http.Get("http://www.baidu.com")
+//	//	defer response.Body.Close()
+//
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	body, err := ioutil.ReadAll(response.Body)
+//	if err != nil {
+//		panic(err)
+//	}
+//	fmt.Println(string(body))
+//}
 
 func main() {
 	//	SendHttpRequestDemo()
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		io.WriteString(writer, "Hello Golang\n")
+		if _, err := io.WriteString(writer, "Hello Golang\n"); err != nil {
+			panic(err)
+		}
 	})
 
 	type User struct {
@@ -57,7 +59,9 @@ func main() {
 			return
 		}
 		writer.Header().Set("Content-Type", "application/json")
-		writer.Write(userJson)
+		if _, err := writer.Write(userJson); err != nil {
+			panic(err)
+		}
 	})
 
 	http.HandleFunc("/image", func(writer http.ResponseWriter, request *http.Request) {
